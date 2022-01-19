529429896a22ef78afd6831cefba7998f9d38e73
Kuboczoch (Jakub Pokorski) <jakub.pokorski@protonmail.com>
2022-01-19 02:36:58 +0100

    Added tests

--- src/containers/HomePage/HomePageView.tsx
+++ src/containers/HomePage/HomePageView.tsx
@@ -3,14 +3,12 @@ import React from 'react'
 import { IHomePageProps } from '../../pages'
 import { IHomePageStateProps } from './useHomePage'
 
-import HomePage from '../../components/blocks/HomePage'
-
 interface IHomePageViewProps extends IHomePageProps, IHomePageStateProps {}
 
 const HomePageView = ({}: IHomePageViewProps) => (
-  <HomePage>
+  <div>
     <h1>next-popular-website-starter</h1>
-  </HomePage>
+  </div>
 )
 
 export default HomePageView
