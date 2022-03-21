1b776fb1ce59bdf855fee38087a55feafa36daed
Jakub Pokorski <jakub.pokorski@protonmail.com>
2022-03-21 22:18:29 +0100

    Keycloak integration via next-auth (#11)
    * Keycloak integration via next-auth #10

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,27 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles'
 import { ThemeProvider } from '@emotion/react'
+import { SessionProvider } from 'next-auth/react'
 
 import GlobalStyle from '../components/elements/Layout/GlobalStyle'
 import CustomThemeProvider, { CustomThemeContext } from '../providers/CustomThemeProvider'
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
-    <CustomThemeProvider>
-      <CustomThemeContext.Consumer>
-        {(customThemeContext) => (
-          <MuiThemeProvider theme={customThemeContext.theme}>
-            <ThemeProvider theme={customThemeContext.theme}>
-              {GlobalStyle}
-              <Component {...pageProps} />
-            </ThemeProvider>
-          </MuiThemeProvider>
-        )}
-      </CustomThemeContext.Consumer>
-    </CustomThemeProvider>
+    <SessionProvider session={session}>
+      <CustomThemeProvider>
+        <CustomThemeContext.Consumer>
+          {(customThemeContext) => (
+            <MuiThemeProvider theme={customThemeContext.theme}>
+              <ThemeProvider theme={customThemeContext.theme}>
+                {GlobalStyle}
+                <Component {...pageProps} />
+              </ThemeProvider>
+            </MuiThemeProvider>
+          )}
+        </CustomThemeContext.Consumer>
+      </CustomThemeProvider>
+    </SessionProvider>
   )
 }
 
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from 'next-auth'
+import KeycloakProvider from 'next-auth/providers/keycloak'
+
+export default NextAuth({
+  providers: [
+    KeycloakProvider({
+      // @ts-ignore must be string
+      clientId: process.env.KEYCLOAK_CLIENT_ID,
+      // @ts-ignore must be string
+      clientSecret: process.env.KEYCLOAK_SECRET,
+      issuer: process.env.KEYCLOAK_ISSUER
+    })
+  ]
+})
